d5da34e7a2c7a32b312e0dbfc4255ade1254fcd4
Astejus <ivanovasastijus@gmail.com>
2024-05-03 12:57:57 +0300

    Labaratorinis darbas

--- R/2-visualization.R
+++ R/2-visualization.R
@@ -1,1 +1,40 @@
 library(tidyverse)
+library(ggplot2)
+
+#2.1 užduotis
+
+data = readRDS("../data/692000.rds")
+png(file="../img/1_Graph.png", width = 1500, height = 1000)
+Histo = hist(data$avgWage, breaks = 200, main = paste("Vidutinio atlyginimo histograma"),
+             xlab = "Atlyginimas", ylab = "Kiekis", col = 'green')
+dev.off()
+
+#2.2 užduotis
+
+top5 = data %>%
+  group_by(name) %>%
+  summarise(top5i = max(avgWage)) %>%
+  arrange(desc(top5i)) %>%
+  head(5)
+
+Histo2 = data %>%
+  filter(name %in% top5$name) %>%
+    mutate(Months = ym(month)) %>%
+    ggplot(aes(x = Months, y = avgWage, color = name)) + geom_line() +
+    labs(title = "5 įmonės, kurių vidutinės algos didžiausios", x = "Mėnesiai", y = "Vidutinis atlyginimas")
+    ggsave("../img/Graph_2.png", Histo2, width = 2600, height = 1300, units = ("px"))
+    
+#2.3 užduotis
+    
+insured = data %>%
+  filter(name %in% top5$name) %>%
+  group_by(name) %>%
+  summarise(MostInsured = max(numInsured)) %>%
+  arrange(desc(MostInsured))
+
+insured$name = factor(insured$name,levels = insured$name[order(insured$MostInsured, decreasing = T)] )
+  
+Histo3 = insured%>%
+  ggplot(aes(x = name, y = MostInsured, fill = name)) +geom_col()+
+  labs(title = "5 įmonės, kurių vidutinės algos didžiausios", x = "Mėnesiai", y  = "Apdraustieji")
+ggsave("../img/Graph_3.png", Histo3, width = 4000, height = 2500, units = ("px"))
--- app/app.R
+++ app/app.R
@@ -1,1 +1,36 @@
 library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(jsonlite)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Veiklos kodas 692000"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput("Įmonė",
+                     "Pasirinkite įmonę",choices = NULL)),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+server <- function(input, output,session) {
+  data = readRDS('../data/692000.rds')
+  updateSelectizeInput(session, "Įmonė", choices = data$name, server = T)
+  output$plot = renderPlot (
+    data%>%
+      filter( name == input$Įmonė) %>%
+      ggplot(aes(x = ym(month), y=numInsured)) + geom_line()+ geom_point() + theme_grey() + labs(x = "Mėnesis", y="Apdraustuju skaicius")
+  )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
